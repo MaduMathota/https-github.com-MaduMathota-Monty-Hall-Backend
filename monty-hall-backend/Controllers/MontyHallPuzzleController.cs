fade147d176ba277c2d3a3663a549aa9fb6805c3
MaduMathota <184102v@uom.lk>
2024-02-16 23:01:54 +0530

    initial changes

--- monty-hall-backend/Controllers/MontyHallPuzzleController.cs
+++ monty-hall-backend/Controllers/MontyHallPuzzleController.cs
@@ -1,24 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MontyHallBackend;
 
-namespace monty_hall_backend.Controllers
-        {
-    [Route("api/[controller]")]
-        [ApiController]
-        public class MontyHallPuzzleController : ControllerBase
-        {
-            [HttpGet("Simulation/Montyhall")]
-            public ActionResult RunSimulation(int simulations, bool changeDoor)
-            {
-                var simulation = new MontyHallPuzzle(simulations, changeDoor);
-                simulation.RunSimulations();
-                return Ok(new
-                {
-                    SwitchWinRate = simulation.GetSwitchWinRate(),
-                    StayWinRate = simulation.GetStayWinRate()
 
-                });
-            }
+namespace monty_hall_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MontyHallPuzzleController : ControllerBase
+    {
+        [HttpGet("Simulation/api")]
+        public ActionResult RunSimulation(int simulations, bool changeDoor)
+        {
+            var simulation = new MontyHallPuzzle(simulations, changeDoor);
+            simulation.RunSimulations();
+            return Ok(new
+            {
+                SwitchWinRate = simulation.GetSwitchWinRate(),
+                StayWinRate = simulation.GetStayWinRate()
+
+            });
         }
     }
+}
